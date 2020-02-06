1e590c8572e05a120bdb360b882a1187b0c1e6c1
Michelle <michedomingo@gmail.com>
2020-02-06 07:40:20 +0000

    Write a C program without printf or puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+#include "string.h"
+#include "unistd.h"
+
+/**
+ *main - print a string without printf or puts
+ *Return: 1
+ */
+
+int main(void)
+{
+	int i;
+
+	i = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", i);
+	return (0);
+}

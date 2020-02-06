8f8655f41851deddbf8d558fb8fd8cd892555a8a
Michelle <michedomingo@gmail.com>
2020-02-06 22:39:26 +0000

    Write C program to assign random number to n with specific output per task

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * Description: Print whether n is positive, negative, or 0
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n, lastDigit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastDigit = n % 10;
+
+	if (n > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+	}
+	else if (n < 6)
+	{
+		printf("Last digit of %d is %d ", n, lastDigit);
+		printf("and is less than 6 and not 0\n");
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+	}
+	return (0);
+}

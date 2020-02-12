d4f880da0e0fc5304b246666fb35a976739c19be
Michelle <michedomingo@gmail.com>
2020-02-12 02:25:20 +0000

    Write a function that adds two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - adds two integers
+ * @a: integer to add
+ * @b: integer to add
+ * Return: sum of integers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}

3821fe735f4e12c430fef516dfc38bc2f3d1749c
Michelle <michedomingo@gmail.com>
2020-03-04 06:50:37 +0000

    Write a function that allocates memory for pointer to 2d array of integers

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * alloc_grid - allocate memory for pointer to 2d array of integers
+ * @width: int type 2d array, row
+ * @height: int type 2d array, column
+ * Return: pointer to grid
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **ptr;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	ptr = malloc(height * sizeof(*ptr));
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		ptr[i] = malloc(width * sizeof(int));
+		if (ptr[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(ptr[j]);
+			free(ptr);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			ptr[i][j] = 0;
+	}
+	return (ptr);
+}

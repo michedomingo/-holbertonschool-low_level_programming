be2d507ab5adc71548364940418dbc91b2201a49
Michelle <michedomingo@gmail.com>
2020-03-05 21:05:37 +0000

    Update from <= to ==, line 15

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *ptr;
 	unsigned int index;
 
-	if ((nmemb <= 0) || (size <= 0))
+	if ((nmemb == 0) || (size == 0))
 		return (NULL);
 
 	ptr = malloc(nmemb * size);

f88c49321dd704fa737dda8e9f6d6efe52370108
Michelle <michedomingo@gmail.com>
2020-03-05 22:36:38 +0000

    Update condition nmemb * size, line 22

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -19,7 +19,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (ptr == NULL)
 		return (NULL);
 
-	for (index = 0; index < nmemb; index++)
+	for (index = 0; index < (nmemb * size); index++)
 		ptr[index] = 0;
 
 	return (ptr);

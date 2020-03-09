96faa03a9d27e22c87a5dc1e583999e85cafab09
Michelle <michedomingo@gmail.com>
2020-03-09 20:14:14 +0000

    Add _strlen and _strcpy prototypes, include stdlib/stdio libraries

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct dog - definition for a struct called dog
@@ -20,5 +21,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
 
 #endif

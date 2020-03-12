b728c04d50ea5cc9c36b27a67814c5bd7369f94d
Michelle <michedomingo@gmail.com>
2020-03-12 06:49:53 +0000

    Add description

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,7 +4,9 @@
  * print_name - function that prints a name
  * @name: pointer to char string
  * @f: pointer to fuction
+ * Description - write a function that prints a name
  */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (*f == NULL)

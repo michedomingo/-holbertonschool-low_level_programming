7d8a804d29fef005e54e8ba73fa8877b023f243e
Michelle <michedomingo@gmail.com>
2020-03-12 07:47:17 +0000

    Write a function that selects the correct function to perform the operation asked by the user

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,25 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - selects the correct function to perform
+ * @s: operator passed from main
+ * Return: pointer to function that corresponds to operator
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (ops[i].op != NULL && strcmp(ops[i].op, s) != 0)
+	{
+		i++;
+	}
+	return (ops[i].f);
+}
